99d6fde54335fcb995002042fc48d5544f9fa67d
root <xacobo@ticgal.com>
2021-05-04 12:22:15 +0200

    Filter is_deleted

--- report/coststech/coststech.php
+++ report/coststech/coststech.php
@@ -49,7 +49,7 @@ $report->setColumns([
 	)
 ]);
 
-$query="SELECT glpi_tickets.entities_id,glpi_users.profiles_id,SUM(glpi_tickettasks.actiontime) AS duration, SUM(`glpi_ticketcosts`.`actiontime` * `glpi_ticketcosts`.`cost_time`/3600 + `glpi_ticketcosts`.`cost_fixed` + `glpi_ticketcosts`.`cost_material`) AS totalcost FROM glpi_tickettasks INNER JOIN glpi_tickets ON glpi_tickets.id=glpi_tickettasks.tickets_id INNER JOIN glpi_plugin_costs_tasks ON glpi_plugin_costs_tasks.tasks_id=glpi_tickettasks.id INNER JOIN glpi_ticketcosts ON glpi_ticketcosts.id=glpi_plugin_costs_tasks.costs_id INNER JOIN glpi_users ON glpi_users.id=glpi_tickettasks.users_id_tech WHERE glpi_tickets.status>=".Ticket::SOLVED." ";
+$query="SELECT glpi_tickets.entities_id,glpi_users.profiles_id,SUM(glpi_tickettasks.actiontime) AS duration, SUM(`glpi_ticketcosts`.`actiontime` * `glpi_ticketcosts`.`cost_time`/3600 + `glpi_ticketcosts`.`cost_fixed` + `glpi_ticketcosts`.`cost_material`) AS totalcost FROM glpi_tickettasks INNER JOIN glpi_tickets ON glpi_tickets.id=glpi_tickettasks.tickets_id INNER JOIN glpi_plugin_costs_tasks ON glpi_plugin_costs_tasks.tasks_id=glpi_tickettasks.id INNER JOIN glpi_ticketcosts ON glpi_ticketcosts.id=glpi_plugin_costs_tasks.costs_id INNER JOIN glpi_users ON glpi_users.id=glpi_tickettasks.users_id_tech WHERE glpi_tickets.status>=".Ticket::SOLVED." AND glpi_tickets.is_deleted=0 ";
 $group=" GROUP BY glpi_tickets.entities_id,glpi_users.profiles_id";
 
 $query .= $report->addSqlCriteriasRestriction();

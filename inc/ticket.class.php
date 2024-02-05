0c699b452832b3ce4976f31fd42bdf264cf3da87
FrancisTicgal <francisco@ticgal.com>
2024-02-05 14:26:18 +0100

    update headers

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4,23 +4,23 @@
  * -------------------------------------------------------------------------
  * Costs plugin for GLPI
  * Copyright (C) 2018-2024 by the TICgal Team.
- * 
+ *
  * https://github.com/ticgal/costs
  * -------------------------------------------------------------------------
  * LICENSE
- * 
+ *
  * This file is part of the Costs plugin.
- * 
+ *
  * Costs plugin is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Costs plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Costs. If not, see <http://www.gnu.org/licenses/>.
  * -------------------------------------------------------------------------

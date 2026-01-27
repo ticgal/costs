f811854d28d9cd3ad75e3ce5699892a510803fa3
Xacobo Freire <xacobo@ticgal.com>
2026-01-27 16:22:47 +0100

    new beta

--- setup.php
+++ setup.php
@@ -36,7 +36,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_COSTS_VERSION', '4.0.0-beta.1');
+define('PLUGIN_COSTS_VERSION', '4.0.0-beta2');
 // Minimal GLPI version, inclusive
 define("PLUGIN_COSTS_MIN_GLPI", "11.0");
 // Maximum GLPI version, exclusive
